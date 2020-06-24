3b902c4143386b4012d6dd63be288596c50fcd48
Yugi10 <67090447+Yugi10@users.noreply.github.com>
2020-06-24 14:07:35 +0530

    Update SchoolAdministration.py

--- SchoolAdministration.py
+++ SchoolAdministration.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import csv
 
 def info(students) :
@@ -34,3 +28,61 @@ if __name__ == "__main__" :
             else:
                 count += 1
 
+"""
+
+OUTPUT :
+
+Please enter the information of student 1 in the given format only :
+
+
+Name, Age, Mobile_Number, Email_ID :
+Yugi, 19, 915476254, yugiyugi@gmail.com
+
+Name : Yugi 
+Age :  19 
+Mobile_Number :  915476254 
+Email_ID :  yugiyugi@gmail.com
+
+
+Please check if the entered information is correct [type yes/no] :
+yes
+
+Do you want to enter information for another student [type yes/no] ?
+yes
+
+Please enter the information of student 2 in the given format only :
+
+
+Name, Age, Mobile_Number, Email_ID :
+Harry, 25, 988462496, harry@yahoo.com
+
+Name : Harry 
+Age :  25 
+Mobile_Number :  988462496 
+Email_ID :  harry@yahoo.com
+
+
+Please check if the entered information is correct [type yes/no] :
+no
+
+Please enter the information of student 2 in the given format only :
+
+
+Name, Age, Mobile_Number, Email_ID :
+Harry, 25, 988462426, harry@yahoo.co.in
+
+Name : Harry 
+Age :  25 
+Mobile_Number :  988462426 
+Email_ID :  harry@yahoo.co.in
+
+
+Please check if the entered information is correct [type yes/no] :
+yes
+
+Do you want to enter information for another student [type yes/no] ?
+no
+
+Thank You!
+
+"""
